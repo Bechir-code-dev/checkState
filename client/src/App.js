965f88fbf15d0c1558b3691b60aae6d82cd00b64
bechir <bechir>
2025-03-10 23:04:57 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       person: {
         fullName: "Larry Page",
         bio: "A passionate software developer.",
-        imgSrc: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/59d2866a671de711f6efeebb19b97104a69341f8_254x191.jpg?w=255',
+        imgSrc:
+          "https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/59d2866a671de711f6efeebb19b97104a69341f8_254x191.jpg?w=255",
         profession: "Software Engineer",
       },
       shows: false,
-      timeSinceMount: 0,
+      chrono: 0,
     };
-  }
-  render(){
-    
   }
 
   
+  toggleShow = () =>{
+    this.setState({shows : !this.state.shows} )
+  }
+// hedhi function bch tbadel shows 
+
+
+  render() {
+    const { person, shows } = this.state;
+    return (
+      <div>
+        <button onClick={this.toggleShow}>toggleShow</button>
+        {shows && (
+          <div>
+            <img src={person.imgSrc} alt="larry" />
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <p>{person.profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 export default App;
 
 // render nesta3mlouha wakteli component's state wala props change
+// {shows ? 'Hide Profile' : 'Show Profile'}
